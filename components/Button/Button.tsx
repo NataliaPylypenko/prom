bf74cdf43f4293fc387fbd21b1a1f2dcf9669bcc
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-07-28 15:45:20 +0300

    svg component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,8 +1,9 @@
 import { ButtonProps } from "./Button.props";
 import styles from './Button.module.css';
-import cn from 'classnames'
+import ArrowIcon from './arrow.svg';
+import cn from 'classnames';
 
-export const Button = ({ appearance, children, className, ...props }: ButtonProps): JSX.Element => {
+export const Button = ({ appearance, arrow = 'none', children, className, ...props }: ButtonProps): JSX.Element => {
     return (
         <button className={ cn(styles.button, className, {
                 [styles.primary]: appearance == 'primary',
@@ -11,6 +12,11 @@ export const Button = ({ appearance, children, className, ...props }: ButtonProp
             {...props}
         >
             {children}
+            {arrow != 'none' && <span className={cn(styles.arrow, {
+                [styles.down]: arrow == 'down'
+            })}>
+                <ArrowIcon />
+            </span>}
         </button>
     );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home(): JSX.Element {
   return (
       <>
           <Htag tag='h1'>Курсы по Photoshop</Htag>
-          <Button appearance='primary'>Button</Button>
+          <Button appearance='primary' arrow="right">Button</Button>
           <Button appearance='ghost'>Button</Button>
       </>
   );
