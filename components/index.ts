b6d0a0def76c4665287966a360487cd0055f0e18
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-08-09 11:50:07 +0300

    rating component

--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,4 @@ export * from './Htag/Htag';
 export * from './Button/Button';
 export * from './P/P';
 export * from './Label/Label';
+export * from './Rating/Rating';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,30 @@
-import React from "react";
-import {Button, Htag, Label, P} from "../components";
+import React, {useEffect, useState} from "react";
+import {Button, Htag, Label, P, Rating} from "../components";
 
 export default function Home(): JSX.Element {
-  return (
-      <>
-          <Htag tag='h1'>Heading level 1</Htag>
-          <Htag tag='h2'>Heading level 2</Htag>
-          <Htag tag='h3'>Heading level 3</Htag>
+    const [rating, setRating] = useState<number>(4);
 
-          <Button appearance='primary' arrow="right">Button Primary</Button>
-          <Button appearance='ghost'>Button Ghost</Button>
+    return (
+        <>
+            <Htag tag='h1'>Heading level 1</Htag>
+            <Htag tag='h2'>Heading level 2</Htag>
+            <Htag tag='h3'>Heading level 3</Htag>
 
-          <P size='l'>Large</P>
-          <P>Medium</P>
-          <P size='s'>Small</P>
+            <Button appearance='primary' arrow="right">Button Primary</Button>
+            <Button appearance='ghost'>Button Ghost</Button>
 
-          <Label size='m' color='red'>figma.com</Label>
-          <Label color='primary'>Web дизайн</Label>
-          <Label color='success'>-10 000 ₽ </Label>
-          <Label>Ghost</Label>
-      </>
-  );
+            <P size='l'>Large</P>
+            <P>Medium</P>
+            <P size='s'>Small</P>
+
+            <Label size='m' color='red'>figma.com</Label>
+            <Label color='primary'>Web дизайн</Label>
+            <Label color='success'>-10 000 ₽ </Label>
+            <Label>Ghost</Label>
+
+            <Rating rating={4} isEditable />
+            <Rating rating={3} />
+            <Rating rating={rating} isEditable setRating={setRating} />
+        </>
+    );
 }
